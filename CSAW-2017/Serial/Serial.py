af26c5c87ca23b96912c5701a68c6306da5c72f9
bruceshenzk <bruceshenzk@gmail.com>
2017-09-23 10:47:10 -0400

    add files

---
+++ CSAW-2017/Serial/Serial.py
@@ -0,0 +1,49 @@
+from pwn import *
+
+bytes = []
+def count (input):
+	c = 0
+	for i in range(8):
+		if input[i] == '1':
+			c += 1
+	return c
+
+def correct (input):
+	byte = input[1:9]
+	start = input[0]
+	end = input[10]
+	purity = input[9]
+	print start, byte, purity, end
+	if start != '0' or end != '1':
+		return "0"
+	elif (count(byte)+1) % 2 != int(purity):
+		bytes.append(byte)
+		return "1"
+	else:
+		return "0"
+
+r = remote("misc.chal.csaw.io", 4239)
+
+print(r.recvuntil("retransmit."))
+r.recvline()
+round = 1
+while True:
+	try:
+		line = r.recvline()[:-1]
+	except:
+		break
+
+	print "DEBUG:", round, line#, correct(line)
+	r.sendline(correct(line))
+	round += 1
+print "flag{".encode("hex")
+flag = ""
+print "".join([chr(int(b,2)) for b in bytes])
+
+# print [b[::-1] for b in bytes]
+# print [int(b[::-1],2) for b in bytes]
+# print [chr(int(b[::-1],2)) for b in bytes]
+# print [chr(int(),2)) for b in bytes]
+
+#r.interactive()
+
---
+++ CSAW-2017/tablEZ/README.md
@@ -0,0 +1,1 @@
+# tablEZ Write Up
